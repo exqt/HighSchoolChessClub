8a9337857cc99a5c86ceaf003179896bad2f4944
exqt <8168124+exqt@users.noreply.github.com>
2026-08-11 05:24:02 +0900

    initial commit

---
+++ Source/HighSchoolChessClub/HighSchoolChessClub.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HighSchoolChessClub.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HighSchoolChessClub, "HighSchoolChessClub" );
